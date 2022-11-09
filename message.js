4e1336b2cd4b87a7e5812e782f647a372f364057
ariel uzal <arieluzal@gmail.com>
2022-11-08 16:45:40 -0800

    a

--- message.js
+++ message.js
@@ -3,18 +3,73 @@ class Message{
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.bearing = 0;
+        this.bearing = this.calculateBearing();;
         this.bearingFixed = false;
+        this.birth = frameCount;
+        this.text = this.getRandomString();
+       // console.log(this.text);
     }
 
     render(){
         push();
-        strokeWeight(3);
-        stroke(255,200,200);
         translate(this.x, this.y);
         rotate(this.bearing);
-        line(0,0,100,0);
+        strokeWeight(3);
+        stroke(255,200,200, map(sin((frameCount - this.birth)*.1), -1, 1, 80, 255));
+        noFill();
+        let diam = 10;
+        ellipse(0,0,diam,diam);
+        if(this.bearingFixed){
+            this.dashedLine();
+        }else{
+            if(dist(this.x,this.y,mouseX,mouseY) > 0){
+                line(0,0,20,0);
+            }
+        }
         pop();
+    }
+
+    update(){
+        if(!this.bearingFixed){
+            this.bearing = this.calculateBearing();
+        }
+
+    }
+
+    mousePressed(){
+        let result = false;
+        if(!this.bearingFixed){
+            this.bearingFixed = true;
+            result = true;
+        }
+        return result;
+    }
+
+    calculateBearing(){
+        return atan2(mouseY - this.y, mouseX - this.x);
+    }
+
+    dashedLine(){
+        push();
+        
+        let dashLength = 5;
+        let lineLength = width;
+        let alt = true;
+        let offset = (frameCount*.5) % (dashLength *2);
+        for(let x = 0 ; x < lineLength ; x+= dashLength){
+            if(alt){
+                line(x + offset ,0,x + dashLength + offset,0);
+            }
+            alt =!alt;
+        }
+        pop();
+
+    }
+
+    getRandomString(){
+        let index = int(random(0,dummyText.length));
+        return(dummyText[index]);
+
     }
 
 
--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 
 let mapImg;
-
+let dummyText;
 let messages;
+let mouseFire;
 
 function preload(){
   mapImg = loadImage("assets/LosAngeles.png");
-
+  dummyText = loadStrings("assets/texto.txt");
 }
 
 function setup() {
@@ -22,14 +23,44 @@ function draw() {
 
 function renderMessages(){
   for (let i = 0 ; i < messages.length ; i++){
+    messages[i].update();
     messages[i].render();
   }
 }
 
 function mousePressed(){
-  createMessage(mouseX, mouseY);
+  if (mouseButton === LEFT) {
+    mouseFire = millis();
+  }
+}
+
+function mouseReleased(){
+  if (mouseButton === LEFT && millis() - mouseFire < 200) {
+    if(readyForNewMessage()){
+      createMessage(mouseX, mouseY);
+    }else{
+      for (let i = 0 ; i < messages.length ; i++){
+        messages[i].mousePressed();
+      }
+    }
+  }
+  
 }
 
 function createMessage(x, y){
-  let msg = new Message(x, y);
+  
+    let msg = new Message(x, y);
+    messages.push(msg);
+  
+}
+
+function readyForNewMessage(){
+  let ready = true;
+  for (let i = 0 ; i < messages.length ; i++){
+    if(!messages[i].bearingFixed){
+      ready = false;
+      break;
+    }
+  }
+  return ready;
 }
